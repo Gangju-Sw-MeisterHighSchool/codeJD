4c02e7c6204e7fb3666ffd176985c6b8e49c1fb3
las-devil <haho6629@gmail.com>
2017-07-18 12:38:27 +0900

    batebace delete && selete

--- check/src/check/database.java
+++ check/src/check/database.java
@@ -14,71 +14,76 @@ import com.mysql.*;
 
 
 
-
-class database {
-		public static void main(String[] args) {
-			Connection conn = null;                         
-
-			PreparedStatement pstmt = null;
-
-
-
-			DriverManager out;
-			try{
-
-			String url = "jdbc:mysql://localhost:3306/code";
-
-			String id = "root";                            
-
-			String pw = "123456";                          
-			
-			Class.forName("com.mysql.jdbc.Driver");        
-
-			conn=DriverManager.getConnection(url,id,pw);   
-			
+class database {		
+	public void da() {
+		Connection conn = null;                         
+		PreparedStatement pstmt = null;
+		DriverManager out;
+		try{
+		String url = "jdbc:mysql://localhost:3306/code";
+		String id = "root";                            
+		String pw = "123456";                          
+		Class.forName("com.mysql.jdbc.Driver");        
+		conn=DriverManager.getConnection(url,id,pw);   
+		
+		String sql1= "select * from pushCode";
+		pstmt = conn.prepareStatement(sql1);            
+		ResultSet rs = pstmt.executeQuery();     			
+		while(rs.next()) 
+		{
+			String asd = rs.getString("souce");
+			System.out.println(asd);
+		}
+		
+		
+		/*
 			String sql = "insert into pushCode values(?,?,?,?)";      
-
 			pstmt = conn.prepareStatement(sql);                       
-
 			pstmt.setInt(1,456);
 			pstmt.setTimestamp(2, new Timestamp(System.currentTimeMillis()));   
-			pstmt.setString(3,"las");
-
+			pstmt.setString(3,"test");
 			pstmt.setInt(4,132);
-
-			pstmt.executeUpdate();                                        // 쿼리를 실행한다.
-			String sql1= "select * from pushCode where name = 456";                        // sql 쿼리
-
+			pstmt.executeUpdate();   
+		
+// 쿼리를 실행한다.
+			
+			
+			for(int i = 0;i<5;i++)
+			{
+				
+				String sql2= "delete from pushCode where souce='test'";                        // sql 쿼리
+				pstmt = conn.prepareStatement(sql2);       
+				pstmt.execute(sql2);                                        // 쿼리를 실행한다.
+			}
+			
+			
+			String sql1= "select * from pushCode";                        // sql 쿼리
+			//pstmt.setInt(1,456);
 			pstmt = conn.prepareStatement(sql1);            
 
-
-
+			
 			ResultSet rs = pstmt.executeQuery();     
-			System.out.println(rs);
+			boolean is = rs.next();
+			System.out.println(is);
 
-
-			
-			
-			DriverManager.println("add 0");
-
-			
-
-			}catch(Exception e){
-
-			e.printStackTrace();
-
-			DriverManager.println("addx");
-
-			}finally{                                                            // 쿼리가 성공 또는 실패에 상관없이 사용한 자원을 해제 한다. (순서중요)
-
-			if(pstmt != null) try{pstmt.close();}catch(SQLException sqle){}            // PreparedStatement 객체 해제
-
-			if(conn != null) try{conn.close();}catch(SQLException sqle){}            // Connection 해제
-
+			while(rs.next()) 
+			{
+				String asd = rs.getString("souce");
+				System.out.println(asd);
 			}
+			DriverManager.println("add 0");
+*/
+		}catch(Exception e){
 
-		}
+		e.printStackTrace();
 
-		
+		DriverManager.println("addx");
+
+		}finally{                                                            // 쿼리가 성공 또는 실패에 상관없이 사용한 자원을 해제 한다. (순서중요)
+
+		if(pstmt != null) try{pstmt.close();}catch(SQLException sqle){}            // PreparedStatement 객체 해제
+
+		if(conn != null) try{conn.close();}catch(SQLException sqle){}            // Connection 해제
 	}
-
+}
+}
--- check/src/check/main.java
+++ check/src/check/main.java
@@ -1,11 +1,80 @@
 package check;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.io.*;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		database code = new database();
 		
+		Connection conn = null;                         
+		PreparedStatement pstmt = null;
+		DriverManager out;
+		try{
+		String url = "jdbc:mysql://localhost:3306/code";
+		String id = "root";                            
+		String pw = "123456";                          
+		Class.forName("com.mysql.jdbc.Driver");        
+		conn=DriverManager.getConnection(url,id,pw);   
+
+/*
+		String sql = "insert into pushCode values(?,?,?,?)";
+		PreparedStatement pstmt1 = conn.prepareStatement(sql);                       
+		pstmt1.setInt(1,001);
+		pstmt1.setTimestamp(2, new Timestamp(System.currentTimeMillis()));   
+		pstmt1.setString(3,"test");
+		pstmt1.setInt(4,001);
+		pstmt1.executeUpdate();	
+	*/	
+		String sql1= "select * from pushCode";
+		pstmt = conn.prepareStatement(sql1);            
+		ResultSet rs = pstmt.executeQuery();     			
+		while(rs.next()) 
+		{
+			String asd = rs.getString("souce");
+			outPutAu(asd);
+			//exeq();
+			System.out.println(asd);
+		}
+
+		}catch(Exception e){
+		e.printStackTrace();
+		DriverManager.println("addx");
+		}finally{     
+		if(pstmt != null) try{pstmt.close();}catch(SQLException sqle){}            // PreparedStatement 객체 해제
+		if(conn != null) try{conn.close();}catch(SQLException sqle){}            // Connection 해제
 	}
 
+		
+		
+	}
+	static void outPutAu(String str) throws IOException
+	{
+		PrintWriter pw = new PrintWriter(new FileWriter("C:/Users/user/Desktop/시스템/play.c"));
+		pw.println(str);
+		pw.close();
+	}
+	static void exeq() throws IOException
+	{
+		PrintWriter pw = new PrintWriter(new FileWriter("C:/Users/user/Desktop/시스템/lunch.bat"));
+		String a="cd C:\\Users\\user\\Desktop\\시스템";
+		pw.println(a+"\r");
+		pw.println("gcc "+"play.c"+"\r\n");
+		pw.close();
+		start("C:/Users/user/Desktop/시스템/lunch.bat"); 
+	}
+	static void start(String command) throws IOException
+	{
+		//String command = "C:/Users/user/Desktop/out555.bat";
+		Runtime ru = Runtime.getRuntime();
+		Process p=ru.exec(command);
+	}
+	
 }
